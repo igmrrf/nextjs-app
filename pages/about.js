02a890ac90a4fb36b1d3b5e5709a7cf81389fff5
The Lazy Dev Otaku <francis.igbiriki.fi@gmail.com>
2020-05-14 19:10:20 +0100

    Let's Go

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+function About() {
+	return (
+		<div>
+			<header>
+				<ul>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="/contact">Contact</a>
+					</li>
+				</ul>
+			</header>
+			<h1>About Page</h1>
+		</div>
+	);
+}
+
+export default About;
